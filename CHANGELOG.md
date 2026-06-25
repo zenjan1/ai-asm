0badd24ebe3ba5ff7fbe88ad13873567d7dadeb7
zenjan1 <zenjan1@users.noreply.github.com>
2026-06-25 21:05:34 +0800

    feat: add gnu_admin, hartebeest_admin, topi_admin (Phase 1060, v1423.0)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Changelog
 
 
+## Phase 1060 - v1423.0 (3842 modules, 124 kernel)
+- Added WASM: gnu_admin (Gnu blue wildebeest), hartebeest_admin (Hartebeest large antelope), topi_admin (Topi fast antelope)
+- Total modules: 3839 → 3842 (blue wildebeest, Coke hartebeest, topi)
+
 ## Phase 1059 - v1422.0 (3839 modules, 124 kernel)
 - Added WASM: oryx_admin (Oryx desert antelope), sable_admin (Sable horned antelope), roan_admin (Roan horse-like antelope)
 - Total modules: 3836 → 3839 (gemsbok oryx, sable antelope, roan antelope)
---
+++ modules/hartebeest_admin/src/main.c
@@ -0,0 +1,31 @@
+/* hartebeest_admin: Hartebeest (Alcelaphus buselaphus) large African antelope (v1.0)
+ * Hartebeest savanna, feeding, breeding, health, market
+ * Features: body_len_cm, body_wt_kg, horn_cm, gallop_speed, hb_idx, age_year
+ */
+#include <stddef.h>
+__attribute__((import_module("host"), import_name("alloc"))) extern unsigned int host_alloc(unsigned int, unsigned int);
+__attribute__((import_module("host"), import_name("print"))) extern void host_print(const char*);
+__attribute__((import_module("host"), import_name("exit"))) extern void host_exit(int);
+__attribute__((import_module("host"), import_name("get_argv"))) extern int host_get_argv(unsigned int, unsigned int);
+#define N 16
+typedef struct{int id,location,bdy_ln,bdy_wt,hn_cm,gl_sp,hb_idx,age_yr,active;} hbee_t;
+typedef struct{int n_savan,n_feed,n_breed,n_health,n_mkt,t_ln,t_wt,t_hn,t_gl,t_hb;} hbee_state_t;
+static hbee_t svl[N],svf[N-2],svb[N-4],svh[N-6],svm[N-6]; static hbee_state_t st; static int init;
+static void ps(const char*s){host_print(s);} static void pi(int v){char b[32];int i=0;if(v<0){b[i++]='-';v=-v;}if(v==0){b[i++]='0';}else{int s=i;while(v>0){b[i++]='0'+(v%10);v/=10;}int e=i-1;while(s<e){char t=b[s];b[s]=b[e];b[e]=t;s++;e--;}}b[i]='\0';host_print(b);}
+static int add(hbee_t*a,int*cnt,int*sum,int mx,int lc,int bl,int bw,int hc,int gs,int hi,int ay){if(*cnt>=mx)return -1;hbee_t*x=&a[*cnt];x->id=*cnt;x->location=lc;x->bdy_ln=bl;x->bdy_wt=bw;x->hn_cm=hc;x->gl_sp=gs;x->hb_idx=hi;x->age_yr=ay;x->active=1;*sum+=bl;(*cnt)++;ps("[HBEE] Hartebeest ");pi(*cnt-1);ps(" lc=");pi(lc);ps(" bl=");pi(bl);ps(" bw=");pi(bw);ps(" hc=");pi(hc);ps(" gs=");pi(gs);ps(" hi=");pi(hi);ps(" ay=");pi(ay);ps("\n");return *cnt-1;}
+int hartebeest_init(void){if(init)return -1;st.n_savan=0;st.n_feed=0;st.n_breed=0;st.n_health=0;st.n_mkt=0;st.t_ln=0;st.t_wt=0;st.t_hn=0;st.t_gl=0;st.t_hb=0;for(int i=0;i<N;i++)svl[i].active=0;for(int i=0;i<N-2;i++)svf[i].active=0;for(int i=0;i<N-4;i++)svb[i].active=0;for(int i=0;i<N-6;i++)svh[i].active=0;for(int i=0;i<N-6;i++)svm[i].active=0;init=1;ps("[HBEE] Hartebeest initialized\n");return 0;}
+int hartebeest_savanna(int lc,int bl,int bw,int hc,int gs,int hi,int ay){return add(svl,&st.n_savan,&st.t_ln,N,lc,bl,bw,hc,gs,hi,ay);}
+int hartebeest_feeding(int lc,int bl,int bw,int hc,int gs,int hi,int ay){return add(svf,&st.n_feed,&st.t_wt,N-2,lc,bl,bw,hc,gs,hi,ay);}
+int hartebeest_breeding(int lc,int bl,int bw,int hc,int gs,int hi,int ay){return add(svb,&st.n_breed,&st.t_hn,N-4,lc,bl,bw,hc,gs,hi,ay);}
+int hartebeest_health(int lc,int bl,int bw,int hc,int gs,int hi,int ay){return add(svh,&st.n_health,&st.t_gl,N-6,lc,bl,bw,hc,gs,hi,ay);}
+int hartebeest_market(int lc,int bl,int bw,int hc,int gs,int hi,int ay){return add(svm,&st.n_mkt,&st.t_hb,N-6,lc,bl,bw,hc,gs,hi,ay);}
+void hartebeest_report(void){ps("[HBEE] Savan: ");pi(st.n_savan);ps(" Ln=");pi(st.t_ln);ps("\nFeed: ");pi(st.n_feed);ps(" Wt=");pi(st.t_wt);ps("\nBreed: ");pi(st.n_breed);ps(" Hn=");pi(st.t_hn);ps("\nHealth: ");pi(st.n_health);ps(" Gl=");pi(st.t_gl);ps("\nMkt: ");pi(st.n_mkt);ps(" Hb=");pi(st.t_hb);ps("\n");}
+void hartebeest_state(void){ps("[HBEE] Savan=");pi(st.n_savan);ps(" Feed=");pi(st.n_feed);ps(" Breed=");pi(st.n_breed);ps(" Health=");pi(st.n_health);ps(" Mkt=");pi(st.n_mkt);ps("\n");}
+int main(void){
+ps("=== Hartebeest Admin Demo ===\n\n");hartebeest_init();
+ps("Hartebeest savanna...\n");for(int i=0;i<N;i++){int lc=(i%5)+1,bl=190+(i*9),bw=170+(i*16),hc=80+(i*5),gs=28+(i*3),hi=(i%8)+1,ay=(i%14)+1;hartebeest_savanna(lc,bl,bw,hc,gs,hi,ay);}
+ps("\nHartebeest feeding...\n");for(int i=0;i<N-2;i++){int lc=(i%4)+2,bl=195+(i*7),bw=175+(i*14),hc=82+(i*4),gs=30+(i*2),hi=(i%6)+1,ay=(i%12)+1;hartebeest_feeding(lc,bl,bw,hc,gs,hi,ay);}
+ps("\nHartebeest breeding...\n");for(int i=0;i<N-4;i++){int lc=(i%3)+1,bl=200+(i*5),bw=180+(i*11),hc=85+(i*4),gs=27+(i*3),hi=(i%5)+1,ay=(i%10)+1;hartebeest_breeding(lc,bl,bw,hc,gs,hi,ay);}
+ps("\nHartebeest health...\n");for(int i=0;i<N-6;i++){int lc=(i%5)+1,bl=185+(i*11),bw=165+(i*20),hc=78+(i*6),gs=25+(i*4),hi=(i%10)+1,ay=(i%8)+1;hartebeest_health(lc,bl,bw,hc,gs,hi,ay);}
+ps("\nHartebeest market...\n");for(int i=0;i<N-6;i++){int lc=(i%4)+1,bl=205+(i*4),bw=185+(i*9),hc=88+(i*3),gs=32+(i*2),hi=(i%4)+1,ay=(i%7)+1;hartebeest_market(lc,bl,bw,hc,gs,hi,ay);}
+ps("\n");hartebeest_report();hartebeest_state();ps("\n=== Demo Complete ===\n");return 0;}
